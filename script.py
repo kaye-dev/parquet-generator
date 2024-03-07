3ec6fa1b12480ddfc6981588dd0339bb15b4adac
kaye-dev <keien.nishikawa.developer@gmail.com>
2024-03-08 01:47:21 +0900

    Parquet ファイルのメタデータの型を変更する対応とメタデータを表示するようにしている

--- script.py
+++ script.py
@@ -7,10 +7,27 @@ print('Parquet start')
 # CSV ファイルを読み込む
 df = pd.read_csv('./data.csv')
 
+# 明示的に型を指定したい場合のみ以下のように列に対して型を指定する
+# 'population' 列のデータ型を int32 に変更 (明示的に指定しないと int64 になる)
+df['population'] = df['population'].astype('int32')
+
 # DataFrame を Parquet ファイルに変換
-table = pa.Table.from_pandas(df)
+table = pa.Table.from_pandas(df, preserve_index=False)
 
 # Parquet ファイルとして保存
 pq.write_table(table, './data.parquet')
+
+# Parquet ファイルのパスを指定
+file_path = './data.parquet'
+
+# Parquet ファイルを読み込む
+parquet_file = pq.ParquetFile(file_path)
+
+# ファイルのスキーマ情報を取得
+schema = parquet_file.schema
+
+# スキーマ情報から列名とデータ型を表示
+for field in schema:
+    print(f'Column name: {field.name}, Data type: {field.physical_type}')
 
 print('Parquet file has been created successfully.')
